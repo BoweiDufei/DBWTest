997291bc061658fdfd51b9cede6a6093af4b7ad2
duxingya <duxingya@duxingyadeiMac.local>
2017-10-18 19:02:32 +0800

    xxx

---
+++ TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by duxingya on 2017/10/18.
+//  Copyright © 2017年 duxingya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by duxingya on 2017/10/18.
+//  Copyright © 2017年 duxingya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
